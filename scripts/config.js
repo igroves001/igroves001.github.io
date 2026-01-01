8b9ce3357b746085d15899a124067b461228ff95
Ian Groves <igroves001@gmail.com>
2026-01-01 21:59:33 +0000

    sss

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 // GitHub API Configuration
 // Token is base64 encoded - decode it before use
-const GITHUB_TOKEN_ENCODED = '2l0aHViX3BhdF8xMUFENFZaNkEwT1kza1p5aGwzTjhwX09GSEtFQ2x4TzRWcWl1Zk5YN0xDMHZrRlVMTFVPV09hbmwyVFZFWFpnV2lJRlEzWDNOREdoU2N4Q3d4';
+const GITHUB_TOKEN_ENCODED = 'Z2l0aHViX3BhdF8xMUFENFZaNkEwT1kza1p5aGwzTjhwX09GSEtFQ2x4TzRWcWl1Zk5YN0xDMHZrRlVMTFVPV09hbmwyVFZFWFpnV2lJRlEzWDNOREdoU2N4Q3d4';
 const GITHUB_REPO = 'igroves001/igroves001.github.io';
 const GITHUB_OWNER = 'igroves001';
 
