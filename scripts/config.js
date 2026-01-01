f2b1d0a609918d83811a4c2844881c4191db230c
Ian Groves <igroves001@gmail.com>
2026-01-01 21:53:17 +0000

    new again again

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 // GitHub API Configuration
 // TODO: Replace with your actual GitHub token and repo
-const GITHUB_TOKEN = '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';
+const GITHUB_TOKEN = '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';
 const GITHUB_REPO = 'igroves001/igroves001.github.io';
 const GITHUB_OWNER = 'igroves001';
 
